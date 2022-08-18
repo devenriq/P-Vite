3e392f1222512aee8afd570baf13cf8fda5d6716
Enrique <octepc.97@gmail.com>
2022-08-18 15:24:23 -0500

    added JSON and watched the file optimized

--- vite-demo/main.js
+++ vite-demo/main.js
@@ -2,12 +2,14 @@ import "./style.css";
 import buttonStyles from "./button.module.css";
 import imageStyles from "./image.module.css";
 import img from "./foto-Diana.png";
+import data from "./data.json";
 
 document.querySelector("#app").innerHTML = `
   <div>
     <h1>Hello Enrique. Nice to see you again!</h1>
     <button id='btn'>Click!</button>
     <img id='img'/>
+    <pre>${JSON.stringify(data.user)}</pre>
   </div>
 
 `;
