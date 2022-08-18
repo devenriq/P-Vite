d0a4689ad23810d773340ff2d4d8408aafd6b0f1
Enrique <octepc.97@gmail.com>
2022-08-18 15:15:28 -0500

    added and played with an image

--- vite-demo/main.js
+++ vite-demo/main.js
@@ -1,23 +1,18 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
+import buttonStyles from "./button.module.css";
+import imageStyles from "./image.module.css";
+import img from "./foto-Diana.png";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
     <h1>Hello Enrique. Nice to see you again!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <button id='btn'>Click!</button>
+    <img id='img'/>
   </div>
+
 `;
 
-setupCounter(document.querySelector("#counter"));
+document.getElementById("btn").className = buttonStyles.btn;
+const imagen = document.getElementById("img");
+imagen.src = img;
+imagen.className = imageStyles.img;
