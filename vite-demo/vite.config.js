88581462bf963f781cfe9e525162079e990656ab
Enrique <octepc.97@gmail.com>
2022-08-18 19:08:31 -0500

    added a new route to the project using the vite config

--- vite-demo/vite.config.js
+++ vite-demo/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
 
 export default defineConfig(({ command, mode }) => {
   const port = 3000;
@@ -9,14 +10,23 @@ export default defineConfig(({ command, mode }) => {
   console.log(env);
 
   if (mode === "development") {
+    return {
+      server: {
+        port,
+      },
+    };
     console.log("Modo de desarrollo");
   } else {
     console.log("Modo produccion");
+    return {
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, "index.html"),
+            help: resolve(__dirname, "help", "help.html"),
+          },
+        },
+      },
+    };
   }
-
-  return {
-    server: {
-      port,
-    },
-  };
 });
